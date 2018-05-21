52d87390dae9c4010b9207481402bbb1e7902e5c
Mario Álvarez <ahoy@m4grio.me>
2018-05-22 00:24:34 +0800

    Remove jekyll as dependence in favour of making the Gemfile mandatory

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-if [ -f Gemfile ]; then
-    bundle install
+if [ ! -f Gemfile ]; then
+    >&2 echo "A Gemfile is required."
+    exit 2
 fi
+
+bundle install
 
 command=$(echo "$@" | awk '{print $1;}')
 options=${@##$command}
+
 case $command in
     'serve')
         command="serve $options --host 0.0.0.0"
